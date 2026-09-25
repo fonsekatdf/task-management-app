f9b746064fd56cab4b49a1ab5734e916e4e75413
Tharindu Dilshan <dilshantharindu261@gmail.com>
2026-09-25 12:18:21 +0530

    feat: connect backend to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config();
+
+// connect to database
+connectDB();
 
 const app = express();
 
